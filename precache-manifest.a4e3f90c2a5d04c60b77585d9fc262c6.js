77d34dd9ff447a9da06afbc16522b2374c95eca9
Mercury Digital <65617908+digitalmercury@users.noreply.github.com>
2020-05-20 22:31:49 +0530

    First commit

---
+++ precache-manifest.a4e3f90c2a5d04c60b77585d9fc262c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db2005c3f8cb0eb5e832c1233c77858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04bb25a71f3445214847",
+    "url": "/static/css/main.4896ce59.chunk.css"
+  },
+  {
+    "revision": "c6069a82ddd5a7d3bb7a",
+    "url": "/static/js/2.5cca9c52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5cca9c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04bb25a71f3445214847",
+    "url": "/static/js/main.0f61ee3d.chunk.js"
+  },
+  {
+    "revision": "233dea908d4de0135036",
+    "url": "/static/js/runtime-main.c8273c4c.js"
+  }
+]);
